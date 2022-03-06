304fe20bc757100b194af7c307cd2ca3009c049a
Shinta3456 <@nakstelkom33.com>
2022-03-06 14:22:01 +0700

    ke 6

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,15 +9,27 @@ use App\Models\Contact;
 
 class ContactController extends Controller
 {
+
+    public function edit($id){
+        $contact = Contact::findOrFail($id);
+        return view('admin/contacts/edit', compact('contact'));
+    }
+
+
     public function index(){
-        return view('contacts', [
-            "title" => "Contacts"
-        ]);
+       $contacts = Contact::paginate(2);
+      return view('admin/contacts/index', compact('contacts'));
+        
     }
 
     public function create(){
+        return view('contacts', [
+            "title" => "Contacts"
+        ]);
 
     }
+     
+    
 
     public function store(Request $request){
         //dd($request->all());
